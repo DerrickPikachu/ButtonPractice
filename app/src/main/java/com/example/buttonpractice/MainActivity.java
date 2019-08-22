ff7ae16d9b33d0a1374f51a0290f1c5ff9b3dcc7
pikaChin <derrfgh4563@gmail.com>
2019-08-22 18:37:16 +0800

    complete bigger and littler button

--- app/src/main/java/com/example/buttonpractice/MainActivity.java
+++ app/src/main/java/com/example/buttonpractice/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.buttonpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.service.autofill.TextValueSanitizer;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int textSize = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnBigger(View v) {
+        TextView text = (TextView) findViewById(R.id.txv);
+        text.setTextSize(++textSize);
+    }
+
+    public void btnLittler(View v) {
+        TextView text = (TextView) findViewById(R.id.txv);
+        text.setTextSize(--textSize);
     }
 }
